f80664ff5f717e6f134f19ac217932d22827861e
Sampsa Kiiskinen <tuplanolla@gmail.com>
2014-09-23 18:13:49 +0300

    Yes to build automation.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Prelude hiding (showList)
 import Data.Functor
+import Data.List (intercalate)
 import System.Environment
 
 -- import Evaluator
@@ -22,13 +24,16 @@ test :: String -> IO ()
 test s = do putStrLn ""
             putStrLn ("Read: " ++ s)
             let t = alexScanTokens s
-            putStrLn ("Tokenized: " ++ show t)
+            putStrLn ("Tokenized: " ++ showList t)
             let p = happyGatherParses t
             putStrLn ("Parsed: " ++ show p)
             let f = formatUnwrapped p
             putStrLn ("Formatted: " ++ f)
             -- x <- evaluateNowhere
             -- putStrLn ("Evaluated: " ++ show x)
+
+showList :: Show a => [a] -> String
+showList x = "[" ++ intercalate ", " (map show x) ++ "]"
 
 loop :: IO ()
 loop = undefined
